b8e0ac292609c4f9d47e500ea60ca56aba77b380
tarasend <tz.taras@gmail.com>
2023-12-15 18:57:31 +0200

    9 span added

---
+++ script.js
@@ -0,0 +1,8 @@
+let kR = document.querySelector('.k-r');
+function start() {
+    for (i = 0; i < 9; i++){
+        let span = document.createElement('span');
+        kR.append(span);
+    };
+};
+start();
