26d4ed4afa4e208a400d8483f99bf15d1b75dc97
tarasend <tz.taras@gmail.com>
2023-12-17 19:40:53 +0200

    animation start

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 let kR = document.querySelector('.k-r');
-function spanCreation() {
-    for (i = 0; i < 8; i++){
+(function spanCreation() {
+    for (i = 0; i < 8; i++) {
         let span = document.createElement('span');
         kR.append(span);
     };
-};
-spanCreation();
+})();
+
+
+let brick = 0;
+let kRspan = document.querySelectorAll('.k-r > span');
+function animation() {
+
+    kRspan.forEach((item) => {
+        item.style.filter = 'contrast(45%)';
+        item.style.boxShadow = 'none';
+    });
+
+    kRspan[brick].style.filter = 'contrast(150%)';
+    kRspan[brick].style.boxShadow = '0px 0px 30px rgb(246, 50, 50)';
+    
+    brick++;
+    
+    if (brick === kRspan.length) brick = 0;
+    
+
+    setTimeout(animation, 700);
+
+
+
+}
+animation();
